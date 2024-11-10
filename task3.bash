6136c1f4d9fc866be02987db67fa87d2d71031f3
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-10 16:32:21 -0500

    Update task3.bash

---
+++ task3.bash
@@ -0,0 +1,9 @@
+# Task 3: Upload changes and deploy changes in GitHub actions
+Commit Changes: First, commit your changes to your local repository.
+
+ git add .
+ git commit -m "Add endpoint to filter sums by result"
+
+Push Changes: Push your changes to your GitHub repository.
+
+ git push origin main
